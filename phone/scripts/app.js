9c9cc7f542768c8c0cec196240e594ef5adc15ef
Ranjan Kumar Das <ranjankrdas09@gmail.com>
2023-11-13 18:33:48 +0530

    custom wave

--- phone/scripts/app.js
+++ phone/scripts/app.js
@@ -829,6 +829,38 @@ $(document).ready(function() {
         container: '#mic',
         waveColor: 'rgb(200, 0, 200)',
         progressColor: 'rgb(100, 0, 100)',
+
+        renderFunction: (channels, ctx) => {
+            const { width, height } = ctx.canvas
+            const scale = channels[0].length / width
+            const step = 10
+        
+            ctx.translate(0, height / 2)
+            ctx.strokeStyle = ctx.fillStyle
+            ctx.beginPath()
+        
+            for (let i = 0; i < width; i += step * 2) {
+              const index = Math.floor(i * scale)
+              const value = Math.abs(channels[0][index])
+              let x = i
+              let y = value * height
+        
+              ctx.moveTo(x, 0)
+              ctx.lineTo(x, y)
+              ctx.arc(x + step / 2, y, step / 2, Math.PI, 0, true)
+              ctx.lineTo(x + step, 0)
+        
+              x = x + step
+              y = -y
+              ctx.moveTo(x, 0)
+              ctx.lineTo(x, y)
+              ctx.arc(x + step / 2, y, step / 2, Math.PI, 0, false)
+              ctx.lineTo(x + step, 0)
+            }
+        
+            ctx.stroke()
+            ctx.closePath()
+        },
     })
 
     const containerOut = document.querySelector('#recordings')
